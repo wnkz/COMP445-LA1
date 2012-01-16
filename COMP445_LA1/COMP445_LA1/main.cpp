3d4ffbcdba653ce549911fc602cef8d592f30825
Grégoire Morpain <gregoire.morpain@gmail.com>
2012-01-16 11:57:19 -0500

    Initial commit

---
+++ COMP445_LA1/COMP445_LA1/main.cpp
@@ -0,0 +1,13 @@
+// COMP445_LA1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	std::cout << "ftpd_tcp starting at host: [sun]" << std::endl;
+	std::cout << "waiting to be contacted for transfering files..." << std::endl;
+	return 0;
+}
+
