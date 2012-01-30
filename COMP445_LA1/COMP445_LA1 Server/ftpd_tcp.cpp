7747514b7f0e0424d47749df55172439419bdc91
Grégoire Morpain <gregoire.morpain@gmail.com>
2012-01-30 13:48:46 -0500

    +server can accept connections

--- COMP445_LA1/COMP445_LA1 Server/ftpd_tcp.cpp
+++ COMP445_LA1/COMP445_LA1 Server/ftpd_tcp.cpp
@@ -4,41 +4,101 @@
 
 ftpd_tcp::ftpd_tcp(void)
 {
+	WSAData wdata;
 
+	if (WSAStartup(MAKEWORD(2, 2), &wdata) != 0) {
+		std::cerr << "WSAStartup() failed" << std::endl;
+	}
+
+	if (InitSocket()) {
+		BindSocket();
+		ListenSocket();
+	} else {
+		std::cerr << "Could not initialize socket" << std::endl;
+	}
+
+	if (getLocalHostname()) {
+		startMessage();
+	} else {
+		std::cerr << "Could not get the local hostname" << std::endl;
+	}
 }
 
 
 ftpd_tcp::~ftpd_tcp(void)
 {
+	WSACleanup();
+}
+
+void ftpd_tcp::run(void)
+{
+	int clientSocket;
+	int clientLenght;
+	struct sockaddr_in clientAddr;
+
+	while (42) {
+		clientLenght = sizeof(sAddr);
+		if ((clientSocket = accept(sSocket, (struct sockaddr*)&clientAddr, &clientLenght)) < 0) {
+			std::cerr << "run(): accept failed" << std::endl;
+		}
+
+		//TODO: Handle connection(s)
+		std::cout << "accept(): new socket: " << clientSocket << std::endl;
+	}
 }
 
 void ftpd_tcp::startMessage(void)
 {
-	std::cout << "ftpd_tcp starting at host: " << this->getHostname().c_str() << std::endl;
+	std::cout << "ftpd_tcp starting at host: " << this->localHostname.c_str() << std::endl;
 	std::cout << "waiting to be contacted for transfering files..." << std::endl;
 }
 
-std::string ftpd_tcp::getHostname(void)
+bool ftpd_tcp::InitSocket(void)
 {
-	WSAData wdata;
-	std::string hostname;
+	if ((sSocket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
+		return false;
+	}
+
+	sPort = ftpd_tcp::LISTEN_PORT;
+	memset(&sAddr, 0, sizeof(sAddr));
+	sAddr.sin_family = AF_INET;               
+	sAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	sAddr.sin_port = htons(sPort);       
+
+	return true;
+}
+
+bool ftpd_tcp::BindSocket(void)
+{
+	if ((bind(sSocket, (struct sockaddr*)&sAddr, sizeof(sAddr))) < 0) {
+		return false;
+	}
+	return true;
+}
+
+bool ftpd_tcp::ListenSocket(void)
+{
+	if ((listen(sSocket, SOMAXCONN)) < 0) {
+		return false;
+	}
+	return true;
+}
+
+bool ftpd_tcp::getLocalHostname(void)
+{
 	char hname[256];
 	int r;
-
-	WSAStartup(MAKEWORD(2, 2), &wdata);
 
 	r = gethostname(hname, sizeof(hname));
 	if (r == SOCKET_ERROR)
 	{
 		//TODO: Error checking.
-		hostname = "Unknown";
+		localHostname = "";
+		return false;
 	}
 	else
 	{
-		hostname = hname;
+		localHostname = hname;
+		return true;
 	}
-
-	WSACleanup();
-
-	return hostname;
  }
--- COMP445_LA1/COMP445_LA1 Server/ftpd_tcp.h
+++ COMP445_LA1/COMP445_LA1 Server/ftpd_tcp.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <iostream>
-#include <winsock2.h>
+#include <WinSock2.h>
 #include <WS2tcpip.h>
 
 #pragma comment(lib, "ws2_32.lib")
@@ -12,6 +12,20 @@ public:
 	ftpd_tcp(void);
 	~ftpd_tcp(void);
 
+	void run(void);
+
+private:
 	void startMessage(void);
-	std::string getHostname(void);
+	bool InitSocket(void);
+	bool BindSocket(void);
+	bool ListenSocket(void);
+	bool getLocalHostname(void);
+
+private:
+	static const unsigned short LISTEN_PORT = 21;
+
+	int sSocket;
+	unsigned short sPort;
+	struct sockaddr_in sAddr;
+	std::string localHostname;
 };
